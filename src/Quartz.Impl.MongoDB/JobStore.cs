780c1cf8a71600ac6f3f98a329a1424acd0f76f2
Joe Waller <waller.joe@gmail.com>
2013-06-18 13:54:16 +0900

    Removed use of deprecated MongoDB APIs

--- src/Quartz.Impl.MongoDB/JobStore.cs
+++ src/Quartz.Impl.MongoDB/JobStore.cs
@@ -96,7 +96,9 @@ namespace Quartz.Impl.MongoDB
 
             lock (lockObject)
             {
-                this.database = MongoDatabase.Create(connectionString);
+                var urlBuilder = new MongoUrlBuilder(connectionString);
+                var client = new MongoClient(urlBuilder.ToMongoUrl());
+                this.database = client.GetServer().GetDatabase(urlBuilder.DatabaseName);
             }
         }
 
@@ -1403,10 +1405,10 @@ namespace Quartz.Impl.MongoDB
             lock (lockObject)
             {
                 // multiple instances management
-                this.Schedulers.Save(new BsonDocument(
-                    new BsonElement("_id", this.instanceId),
-                    new BsonElement("Expires", (SystemTime.Now() + new TimeSpan(0, 10, 0)).UtcDateTime),
-                    new BsonElement("State", "Running")));
+                this.Schedulers.Save(new BsonDocument()
+                    .SetElement(new BsonElement("_id", this.instanceId))
+                    .SetElement(new BsonElement("Expires", (SystemTime.Now() + new TimeSpan(0, 10, 0)).UtcDateTime))
+                    .SetElement(new BsonElement("State", "Running")));
 
                 this.Schedulers.Remove(
                     Query.LT("Expires", SystemTime.Now().UtcDateTime));
